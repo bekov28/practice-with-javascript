fb38955af0308f7e9548a499cb8018ace4f3d32d
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-03-30 14:39:18 +0900

    date and loader practice

--- Restaurant-project/js/script.js
+++ Restaurant-project/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
+  //Tabs
+
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabContents = document.querySelectorAll(".tab_content"),
     tabParents = document.querySelector(".tabheader__items");
@@ -34,4 +36,71 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  //Loader
+  const loaderWrapper = document.querySelector(".loader-wrapper");
+
+  setTimeout(() => {
+    loaderWrapper.style.display = "none";
+  }, 500);
+
+  //Timer
+
+  const deadline = "2025-05-01";
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+
+    const time = Date.parse(endtime) - Date.parse(new Date());
+    if (time < 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      (days = Math.floor(time / (1000 * 60 * 60 * 24))),
+        (hours = Math.floor((time / (1000 * 60 * 60)) % 24)),
+        (minutes = Math.floor((time / (1000 * 60)) % 60)),
+        (seconds = Math.floor((time / 1000) % 60));
+    }
+
+    return {
+      totalTime: time,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function formatNumber(number) {
+    if (number >= 0 && number < 10) {
+      return `0${number}`;
+    } else {
+      return number;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      const time = getTimeRemaining(endtime);
+
+      days.textContent = formatNumber(time.days);
+      hours.textContent = formatNumber(time.hours);
+      minutes.textContent = formatNumber(time.minutes);
+      seconds.textContent = formatNumber(time.seconds);
+
+      if (time.totalTime <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock(".timer", deadline);
 });
