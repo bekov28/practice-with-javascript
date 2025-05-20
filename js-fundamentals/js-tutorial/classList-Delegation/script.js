082fe7f62e5f5f369bc2530f3940f8cc1fbc0382
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-05-20 23:41:17 +0900

    fixes in script.js

---
+++ js-fundamentals/js-tutorial/classList-Delegation/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const btns = document.querySelectorAll("button"); //returns NodeList
+
+//console.log(btns[0].classList.add('yellow'));
+//console.log(btns[0].classList.remove('yellow'));
+
+// btns[0].addEventListener("click", () => {
+//   if (btns[0].classList.contains("red")) {
+//     btns[0].classList.remove("red");
+//   } else {
+//     btns[0].classList.add("red");
+//   }
+// });
+
